64a30d90a8768d8a5705909a32ff16d239b02254
Brad <Bradley.j.baker.1982@gmail.com>
2020-11-09 17:31:07 -0500

    updates

--- Netflix/Config/PluginConfig.cs
+++ Netflix/Config/PluginConfig.cs
@@ -13,14 +13,14 @@ namespace Netflix
         public Guid Guid = new Guid("3A1DD6D1-070C-4B2A-98C2-0D97550518F3"); // Also Needs Set In HTML File
         public string PluginName => "Netflix";
         public string PluginDesc => "Movies from Netflix";
-        
         public string Pin { get; set; }
-
+        public bool Enabled { get; set; }
         public TraktUser TraktUser { get; set; }
 
         public PluginConfig()
         {
             TraktUser = new TraktUser();
+            Enabled = true;
         }
     }
 }
